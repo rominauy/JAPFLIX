d6478a20c94ec30bfe6c13c16441d77c8437e0cf
julietaRecoba <recobajulieta@gmail.com>
2023-10-04 18:39:39 -0300

    Update fetch

--- javascript.js
+++ javascript.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://japceibal.github.io/japflix_api/movies-data.json')
-      .then(response => response.json())
-      .then(data => {
-        // Aquí puedes realizar cualquier operación con los datos obtenidos, como almacenarlos en una variable o procesarlos de alguna manera.
-        // Sin embargo, como no quieres mostrarlos al usuario, no es necesario realizar ninguna acción adicional.
-        console.log(data); // Verificar que los datos se han obtenido correctamente
-      })
-      .catch(error => {
-        // Manejo de errores en caso de que ocurra algún problema durante la solicitud.
-        console.error('Error:', error);
-      });
-  });
+  let fetchedData; fetch('https://japceibal.github.io/japflix_api/movies-data.json')
+    .then(response => response.json())
+    .then(data => {
+      fetchedData = data; console.log(fetchedData);
+    })
+    .catch(error => { console.error('Error:', error); });
+});
+  
+let buscar = document.getElementById("btnBuscar");
